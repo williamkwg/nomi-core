b73b716c355fa8245113c38461eeb907dafcfcff
weiguo.kong <weiguo.kong@qunar.com>
2018-05-15 15:32:28 +0800

    测试

---
+++ src/config/config.js
@@ -0,0 +1,4 @@
+export const port = 3000;
+export const serviceDir = "app/serive"
+export const controllerDir = "app/controller"
+export const middlewareDir = "app/middleware"
---
+++ test/controller/OrderController.js
@@ -0,0 +1,25 @@
+var  RequestMapping = require('../../../../../notations/Controller.js').RequestMapping;
+var  Service = require('../../../../../notations/Service.js').Service;
+
+@RequestMapping({
+    path:"/user/{userId:num}",
+    method:"post",
+    middleware:['md1',"md1"]
+})
+class OrderController{
+
+    @Service('service.orderService')
+    serviceInst
+
+    @RequestMapping({path:"/order/{type:num}",method:"get",})
+    async index(req,res,ctx,paras) {
+        this.serviceInst.loadOrders()
+    }
+
+    @RequestMapping("/order/{type:num}")
+    async index1(req,res,ctx,paras) {
+        this.serviceInst.loadOrders()
+    }
+}
+
+exports.OrderController = OrderController;
